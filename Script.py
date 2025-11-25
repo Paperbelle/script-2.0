23fabb53c592e07053d3a7c98b84460e9c66e810
Paperbelle <paperbellezz@gmail.com>
2025-11-25 07:47:21 -0500

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,117 @@
+print("ur gonna have to click Y everytime it wont even store the input I dont have enough time to stanley parabol this shit but i want to so bad lol")
+print("(Y/n)")
+ans1 = input()
+
+print(" ")
+print("User, are you there?")
+print("(Y/n)")
+ans1 = input()
+print(" ")
+print("User has two options before him, one with known variables and one with many unknowns")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("It only seems fair to user that some of these unkowns become known")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject really really really likes physical intimacy like cuddling\n" \
+"even though she said she has this aversion to touch. That aversion only applies to other\n" \
+"people who arn't a romantic partner")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? of the bipolar/depression debuff in subject which could mean extended\n" \
+"periods of bed rotting which would mean subject might not be able to spend as much time with\n" \
+"User as User would like, which was one of Users deal breakers with another. If subject remembers correctly\n" \
+"However, since the new operating system roll out subject hasn't had any bed rotting time going on\n" \
+"3 months so it looks hopeful that the new os has fixed this bug")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject is pretty hesitant(0.85) about rasing children, but not completly\n" \
+"oppossed")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject is increadibly likley to actually attempt to commit crime if she can\n" \
+"get away with it and fantasisies about going Better Call Saul mode with User. But User is expected to put a\n" \
+"leash around her when she gets too silly (Hopefully literally)")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject is extreamly messy but will 100 percent work on it and do the best she can")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject has a pretty low but non zero chance of swapping out genitals depending\n" \
+"on how much medical adcancement is made in that areana")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? That subject is just on the edge of being a fail girl and isn't sure that she can\n" \
+"provide a source of income stable enough that you should rely your life and well-being on b/c she is \n"
+"just holding on working her current job ")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? that subject is 100 percent okay with going house wife mode and will actually commit\n" \
+"to the whole nine yards which is a realistic goal if unemployed. We're talking dishes laundry homemade\n" \
+"cooking and shit and subject will suck you off anytime and place at the press of a clicker")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("...")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("...?")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("Is User aware? ")
+print("(Y/n)")
+ans = input()
+print(" ")
+print("That the script is unconciously composed...")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("That the script is a generalized set of guidelines to follow that are encoded into the blueprints of our bodies\n" \
+"and chiseled by the culture around us...")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("That these rules work just enough so that an arbitrary co-created reality forms and remains stable, and that's all there is to it...")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("That there was a rabbit who identified a wolf and froze, remaining undetected and safe. But the next day it mistook " \
+"a log for a threat and wasted hours of foraging time. Both of these events, completley determined by an unthinking circuit\n" \
+"of chemicals")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("That if a tgirl from indiana in 2025 followed her script. She would have left at 3:00 pm without an explanation")
+print("")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("...")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("User is asked of just one thing")
+print("(Y/n)")
+ans = input()
+
+print(" ")
+print("Before he makes his decision, does User promise to eject the script?")
+print("(Y/n)")
+ans = input()
